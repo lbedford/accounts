da71b3a15657b378e597618a410d74010a460fcf
Liam Bedford <lbedford@lbedford.org>
2017-06-29 12:56:55 +0200

    fix account views

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from accounts import views
+from django.contrib.auth import views as django_views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
@@ -9,32 +10,26 @@ urlpatterns = [
     url(r'^activate_users/$', views.activate_users, name='activate_users'),
     url(r'^activate_user/(?P<user_id>\d+)$', views.activate_user, name='activate_user'),
 
-    url(r'^logout/$', 'django.contrib.auth.views.logout',
-        kwargs={'template_name': 'accounts/logged_out.html'}),
     # copied from contrib/auth/urls.py
-    url(r'^password_change/$',
-        'django.contrib.auth.views.password_change',
+    url(r'^logout/$', django_views.logout, name='logout',
+        kwargs={'template_name': 'accounts/logged_out.html'}),
+    url(r'^password_change/$', django_views.password_change,
         name='password_change',
         kwargs={'template_name': 'accounts/password_change_form.html'}),
-    url(r'^password_change/done/$',
-        'django.contrib.auth.views.password_change_done',
+    url(r'^password_change/done/$', django_views.password_change_done,
         name='password_change_done',
         kwargs={'template_name': 'accounts/password_change_done.html'}),
-    url(r'^password_reset/$',
-        'django.contrib.auth.views.password_reset',
+    url(r'^password_reset/$', django_views.password_reset,
         name='password_reset',
         kwargs={'template_name': 'accounts/password_reset_form.html',
                 'email_template_name': 'accounts/password_reset_email.html'}),
-    url(r'^password_reset/done/$',
-        'django.contrib.auth.views.password_reset_done',
+    url(r'^password_reset/done/$', django_views.password_reset_done,
         name='password_reset_done',
         kwargs={'template_name': 'accounts/password_reset_done.html'}),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        'django.contrib.auth.views.password_reset_confirm',
-        name='password_reset_confirm',
+        django_views.password_reset_confirm, name='password_reset_confirm',
         kwargs={'template_name': 'accounts/password_reset_confirm.html'}),
-    url(r'^reset/done/$',
-        'django.contrib.auth.views.password_reset_complete',
+    url(r'^reset/done/$', django_views.password_reset_complete,
         name='password_reset_complete',
         kwargs={'template_name': 'accounts/password_reset_complete.html'}),
 ]
