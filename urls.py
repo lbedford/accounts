d5d3330929815fae6e1e081f97d226be400cacf8
Liam Bedford <lbedford@lbedford.org>
2019-07-19 12:11:50 +0200

    update to new auth site standards

--- urls.py
+++ urls.py
@@ -1,6 +1,8 @@
+"""Urls for acccounts app."""
 from django.urls import path
-from accounts import views
 from django.contrib.auth import views as django_views
+
+from accounts import views
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -8,27 +10,37 @@ urlpatterns = [
     path('signup', views.signup, name='signup'),
     path('profile', views.profile, name='profile'),
     path('activate_users', views.activate_users, name='activate_users'),
-    path('activate_user/<int:user_id>', views.activate_user, name='activate_user'),
+    path('activate_user/<int:user_id>',
+         views.activate_user, name='activate_user'),
 
-    path('logout/', django_views.LogoutView.as_view(), name='logout',
-        kwargs={'template_name': 'accounts/logged_out.html'}),
-    path('password_change', django_views.PasswordChangeView.as_view(),
-        name='password_change',
-        kwargs={'template_name': 'accounts/password_change_form.html'}),
-    path('password_change/done', django_views.PasswordChangeDoneView.as_view(),
-        name='password_change_done',
-        kwargs={'template_name': 'accounts/password_change_done.html'}),
-    path('password_reset', django_views.PasswordResetView.as_view(),
-        name='password_reset',
-        kwargs={'template_name': 'accounts/password_reset_form.html',
-                'email_template_name': 'accounts/password_reset_email.html'}),
-    path('password_reset/done', django_views.PasswordResetDoneView.as_view(),
-        name='password_reset_done',
-        kwargs={'template_name': 'accounts/password_reset_done.html'}),
-    path('reset/<uidb64>/<token>/', django_views.PasswordResetConfirmView.as_view(),
-        name='password_reset_confirm',
-        kwargs={'template_name': 'accounts/password_reset_confirm.html'}),
-    path('reset/done', django_views.PasswordResetCompleteView.as_view(),
-        name='password_reset_complete',
-        kwargs={'template_name': 'accounts/password_reset_complete.html'}),
+    path('logout/',
+         django_views.LogoutView.as_view(next_page='registration:index',
+                                         template_name='accounts/logged_out.html'),
+         name='logout'),
+
+    path('password_change',
+         django_views.PasswordChangeView.as_view(
+             template_name='accounts/password_change_form.html'),
+         name='password_change'),
+    path('password_change/done',
+         django_views.PasswordChangeDoneView.as_view(
+             template_name='accounts/password_change_done.html'),
+         name='password_change_done'),
+    path('password_reset',
+         django_views.PasswordResetView.as_view(
+             template_name='accounts/password_reset_form.html',
+             email_template_name='accounts/password_reset_email.html'),
+         name='password_reset'),
+    path('password_reset/done',
+         django_views.PasswordResetDoneView.as_view(
+             template_name='accounts/password_reset_done.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         django_views.PasswordResetConfirmView.as_view(
+             template_name='accounts/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('reset/done',
+         django_views.PasswordResetCompleteView.as_view(
+             template_name='accounts/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
