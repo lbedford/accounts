d35e64b3bae331b784fd0950638224de83a9a48c
Liam Bedford <github@lbedford.org>
2019-07-29 11:39:14 +0200

    fix up some tests

--- tests.py
+++ tests.py
@@ -13,29 +13,32 @@ class LbwUserPropertyTests(TestCase):
         self.user.last_name = "Name"
         self.user.save()
 
-
     def test_cannot_create_lbwuser_without_user(self):
         lbwUser = LbwUser()
         self.assertRaises(IntegrityError, lbwUser.save)
 
     def test_can_create_user_without_profile_picture(self):
-        pass
-        #lbwUser = LbwUser(user)
-        #self.assertIs(True, True)
+        lbwUser = LbwUser(user=self.user)
+        lbwUser.save()
 
     def test_can_create_user_with_profile_picture(self):
-        pass
-        #lbwUser = LbwUser(user, profile_image='/path/to/image')
-        #lbwUser.save()
-        #self.assertIs(True, True)
+        lbwUser = LbwUser(user=self.user, profile_image='/path/to/image')
+        lbwUser.save()
+
+
+class LbwUserMethodTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model()()
+        self.user.first_name = "My"
+        self.user.last_name = "Name"
+        self.user.save()
 
     def test_unicode(self):
         lbwuser = LbwUser(user=self.user)
         lbwuser.save()
         self.assertEqual('My Name', lbwuser.__unicode__())
-
-class LbwUserMethodTests(TestCase):
-
-    def test_unicode(self):
-        pass
-        #self.assertIs(user.__unicode__(), "%s %s" % (first_name, last_name))
+    
+    def test_str(self):
+        lbwuser = LbwUser(user=self.user)
+        lbwuser.save()
+        self.assertEqual('My Name', lbwuser.__str__())
